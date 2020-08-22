d974d0183dfac2cf097af589e799d790d75420df
Allan Cannon <allanjcannon@gmail.com>
2020-08-22 13:26:20 -0500

    added visualization components and visualization poc

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page-wrapper">
+        <ac-viz-container/>
+    </div>
+</template>
+
+<script>
+import visualizationContainerVue from '../components/visualizationContainer.vue';
+export default {
+    components: {
+        'ac-viz-container': visualizationContainerVue,
+    },
+};
+</script>
+
+<style>
+
+</style>
